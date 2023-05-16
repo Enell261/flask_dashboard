9b16b22f97b100c1d52d4a9eca02de66f61884ce
Enell261 <66371138+Enell261@users.noreply.github.com>
2023-05-16 08:26:18 +0000

    added app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
-from flask import Flask
-
+from flask import Flask, render_template
+#initiate app flask incident
 app = Flask(__name__)
+#provide route
+@app.route('/')
+#define a function
+def hello_world():
+  return render_template('home.html')
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
