7b2aca8cdcab0b7e3c946702bbcc1110e9cef7ba
Enell261 <66371138+Enell261@users.noreply.github.com>
2023-05-23 14:41:46 +0000

    Added jobs page

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask, render_template
 #initiate app flask incident
 app = Flask(__name__)
+
+jobs = [{
+  'id':1,
+  'title':'Data Analyst',
+  'location':'Mbabane',
+  'salary': 'E600,000'
+},{
+      'id':2,
+      'title':'Data Scientist',
+      'location':'Mbabane',
+      'salary': 'E800,000'},
+       {
+           'id':3,
+  'title':'Credit Analyst',
+  'location':'Ezulwini',
+  'salary': 'E400,000'
+       }]
 #provide route
 @app.route('/')
 #define a function
-def hello_world():
-  return render_template('home.html')
+def home():
+  return render_template('home.html', jobs=jobs)
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+  app.run(host='0.0.0.0', debug=True, port=8080)
